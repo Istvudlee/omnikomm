733ba6be49b360cf87cc3659b50cabbff1a49786
Istvudlee <istvudlee@mail.ru>
2019-11-17 15:00:38 +0300

    add/fix  form style

--- js/menu_head.js
+++ js/menu_head.js
@@ -3,10 +3,15 @@ let headMenu = document.getElementById('head_menu');
 let click_menu = document.querySelectorAll('.click_menu');
 let child_menu = document.querySelectorAll('.child_menu');
 let imgMenuBurger = document.querySelector('.img_menu_burger');
-let closeBurger = document.querySelector('.close_burger')
-let mobileBurger = document.getElementById('mobile_burger')
-let breadcrumbs = document.getElementById('breadcrumbs')
-click_menu.forEach(item => item.addEventListener('click', clickMenuChild))
+let closeBurger = document.querySelector('.close_burger');
+let mobileBurger = document.getElementById('mobile_burger');
+let breadcrumbs = document.getElementById('breadcrumbs');
+let fullfon = document.querySelector('.fullfon');
+let blockforma = document.querySelector('.blockforma');
+let button_form = document.querySelectorAll('#callback, .prduct_button, #callback2');
+
+click_menu.forEach(item => item.addEventListener('click', clickMenuChild));
+
 
 function clickMenuChild(e) {
 	e.preventDefault()
@@ -40,3 +45,18 @@ imgMenuBurger.addEventListener('click', () => {
 closeBurger.addEventListener('click', () => {
 	mobileBurger.style.display = 'none'
 })
+
+//Forma show 
+button_form.forEach(item => item.addEventListener('click', (e) => {
+	e.preventDefault();
+	blockforma.classList.remove('show-form');
+	fullfon.classList.add('fullopen');
+	blockforma.style.left = document.documentElement.clientWidth/2 - blockforma.offsetWidth/2 +'px';
+	blockforma.style.top = document.documentElement.clientHeight/2 - blockforma.offsetHeight/2 +'px';
+}));
+
+//Close click black fon form 
+fullfon.addEventListener('click', (e) => {
+	e.target.classList.remove('fullopen');
+	blockforma.classList.add('show-form');
+});
