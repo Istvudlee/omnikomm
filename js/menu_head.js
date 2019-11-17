944b8df7025b4c0f5f1a806607616a6adb4f96a5
Istvudlee <istvudlee@mail.ru>
2019-11-17 13:01:43 +0300

    add/fix forma

--- js/menu_head.js
+++ js/menu_head.js
@@ -5,21 +5,21 @@ let child_menu = document.querySelectorAll('.child_menu');
 let imgMenuBurger = document.querySelector('.img_menu_burger');
 let closeBurger = document.querySelector('.close_burger')
 let mobileBurger = document.getElementById('mobile_burger')
-
+let breadcrumbs = document.getElementById('breadcrumbs')
 click_menu.forEach(item => item.addEventListener('click', clickMenuChild))
 
 function clickMenuChild(e) {
 	e.preventDefault()
 	let target = e.target;
 
-	linkActive(target, click_menu, 'link_headMenu_active')
-
-	child_menu.forEach(item => {
-		item.classList.remove('child_menu_show')
-		if (target.dataset.number === item.dataset.number) {
-			item.classList.toggle('child_menu_show')
-		}
-	});
+	linkActive(target, click_menu, 'link_headMenu_active');
+	breadcrumbs.style.display = 'none';
+		child_menu.forEach(item => {
+			item.classList.remove('child_menu_show')
+			if (target.dataset.number === item.dataset.number) {
+				item.classList.toggle('child_menu_show')
+			}
+		});
 
 }
 
